0d0ab854158dd942bbcbd3c6db354533ef6863eb
MobScripts <113401404+MobScripts@users.noreply.github.com>
2022-09-12 19:40:27 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'V3CT0RBUG'
+description 'Vehicle robbery'
+version '1.0'
+
+server_script 'main.lua'
+
+mob_updater 'yes'
+mob_name '^2Vehicle Robbery'
+mob_github 'https://github.com/MobScripts/mob_vehiclerobbery'
+
